d5e7b70f838a562905af22a9b699f72b554e36b6
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-03-17 20:32:06 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+let num5 = 10;
+let num6 = 20;
+let max = Math.max(num5, num6);
